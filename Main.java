3d4f5ebe67e8c98a32be8ed9f61cbfb842a324c2
DiogoGaldom <131724277+DiogoGaldom@users.noreply.github.com>
2023-06-11 23:42:40 +0000

    Add files via upload
    Este programa pede ao usuario algumas notas de 0 a 10, onde ele tem que avaliar com estrutura de fluxo se é boa, muito boa, irregular, ruim.

---
+++ Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("digite uma nota");
+        int nota = input.nextInt();
+
+         switch {
+            case: (nota >= 10) ;
+            System.out.println("muito bom");
+            case: (nota >= 8);
+            System.out.println("bom");
+            case: (nota >=6) ;
+            System.out.println("notavel");
+            case: (nota >=4);
+            System.out.println("suficiente");
+            default:
+            System.out.println("insuficiente");
+
+
+
+        }
+    }
+    }
